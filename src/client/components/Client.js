9f42c625664106adcea81a2607b0ba66cf43eab2
Jonathan Sacramento <jmsmistral@gmail.com>
2019-09-10 00:13:17 +0200

    Add extra client logos (#12)

--- src/client/components/Client.js
+++ src/client/components/Client.js
@@ -20,12 +20,14 @@ const Client = () => (
           <img className="client__logo" src="/img/fedex_logo.svg" alt="Fedex" width="252" height="82"/>
           <img className="client__logo" src="/img/att_logo.svg" alt="ATT" width="252" height="82"/>
           <img className="client__logo" src="/img/dia_logo.svg" alt="DIA" width="252" height="82"/>
-          <img className="client__logo" src="/img/du_logo.png" alt="du" width="110" height="82"/>
+          <img className="client__logo" src="/img/du_logo.png" alt="du" width="100" height="82"/>
           <img className="client__logo" src="/img/rogers_logo.svg" alt="Rogers" width="252" height="82"/>
-          <img className="client__logo" src="/img/unimarc_logo.svg" alt="Unimarc" width="120" height="120"/>
-          <img className="client__logo" src="/img/stc_logo.svg" alt="STC" width="252" height="110"/>
+          <img className="client__logo" src="/img/unimarc_logo.svg" alt="Unimarc" width="82" height="82"/>
+          <img className="client__logo" src="/img/stc_logo.svg" alt="STC" width="180" height="82"/>
           <img className="client__logo" src="/img/telefonica_logo.svg" alt="Telefonica" width="252" height="82"/>
           <img className="client__logo" src="/img/zain_logo.svg" alt="Zain" width="252" height="82"/>
+          <img className="client__logo" src="/img/enercity_logo.svg" alt="Enercity" width="252" height="82"/>
+          <img className="client__logo" src="/img/kyivstar_logo.svg" alt="Kyivstar" width="82" height="82"/>
         </Col>
       </Row>
     </Container>
