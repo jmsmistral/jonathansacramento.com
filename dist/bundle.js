ffde9d94050f860bca01f301c603ce4469b5a223
Jonathan Sacramento <jmsmistral@gmail.com>
2022-01-08 16:08:08 +0400

    Updates

--- dist/bundle.js
+++ dist/bundle.js
@@ -14131,29 +14131,34 @@ module.exports = {
 // List of blog posts to render
 var codeProjects = [{
   id: 1,
-  name: 'sassy',
-  description: 'a simple but powerful language for text interpolation',
-  link: 'https://github.com/jmsmistral/sassy'
+  name: 'lorix',
+  description: 'DataFrame API for Javascript',
+  link: 'https://github.com/jmsmistral/lorix'
 }, {
   id: 2,
-  name: 'data streaming',
-  description: 'A docker-based data streaming PoC with Kafka, Spark, and Zeppelin',
-  link: 'https://github.com/jmsmistral/streaming-docker-poc'
+  name: 'sassy',
+  description: 'simple but powerful language for text interpolation',
+  link: 'https://github.com/jmsmistral/sassy'
 }, {
   id: 3,
+  name: 'transflow',
+  description: 'DAG-based data transformation library',
+  link: 'https://github.com/jmsmistral/transflow'
+}, {
+  id: 4,
+  name: 'data streaming',
+  description: 'docker-based data streaming PoC with Kafka, Spark, and Zeppelin',
+  link: 'https://github.com/jmsmistral/streaming-docker-poc'
+}, {
+  id: 5,
   name: 'tpose',
   description: 'transpose or "pivot" delimited text files in parallel',
   link: 'https://github.com/jmsmistral/tpose'
 }, {
-  id: 4,
+  id: 6,
   name: 'btree',
   description: 'in-memory btree library in C',
   link: 'https://github.com/jmsmistral/btree'
-}, {
-  id: 5,
-  name: 'pguess',
-  description: '[in dev] data type guessing and DDL script generation for delimited text files',
-  link: '#'
 }];
 module.exports = {
   codeProjects: codeProjects
