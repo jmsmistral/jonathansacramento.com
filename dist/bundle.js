1f679ca6281eea0b758d111561dd6a81961be4e3
Jonathan Sacramento <jmsmistral@gmail.com>
2022-01-08 16:19:33 +0400

    Updates

--- dist/bundle.js
+++ dist/bundle.js
@@ -14142,7 +14142,7 @@ var codeProjects = [{
 }, {
   id: 3,
   name: 'transflow',
-  description: '[IN DEV] DAG-based data transformation library',
+  description: '[in dev] DAG-based data transformation library',
   link: 'https://github.com/jmsmistral/transflow'
 }, {
   id: 4,
